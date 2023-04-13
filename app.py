751dba2ac0ceacd2e71aa0796c2ce480ecf3b3a3
prateek <prateek.patel@bricklane.com>
2023-04-13 16:35:58 +0100

    proper naming of stacks

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-from lambda_functions.lambda_stack import LambdaFunctionStack
+from engineering.engineering_stack import DataEngineeringStack
 from ml.ml_lambda_stack import ModelDeployerStack, PipelineOrchestrationStack
 
 app = cdk.App()
 env = {"region": "eu-west-1"}
 
-lfs = LambdaFunctionStack(app, "LambdaFunctionStack", env=env)
+lfs = DataEngineeringStack(app, "DataEngineeringStack", env=env)
 mls = ModelDeployerStack(app, "ModelDeployerStack", env=env)
 pos = PipelineOrchestrationStack(app, "PipelineOrchestrationStack", env=env)
 
