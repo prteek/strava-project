a337501827c8ec250f298a576615c3e23fd093f5
prateek <prateek.patel@bricklane.com>
2023-04-13 19:47:07 +0100

    pipeline orchestration immediately followed by invocation to complete the process

--- ml/ml_stack.py
+++ ml/ml_stack.py
@@ -1,6 +1,7 @@
 import os
 from aws_cdk import aws_lambda, Duration, Stack
 from constructs import Construct
+from aws_cdk import triggers
 
 ENV = os.environ["ENV"]
 
@@ -63,3 +64,9 @@ class PipelineOrchestrationStack(Stack):
             reserved_concurrent_executions=2,
             timeout=Duration.seconds(60),
         )
+
+        # Trigger the function after it is deployed to orchestrate the pipeline
+        trigger = triggers.Trigger(self, "start_orchestration_pipeline",
+                                   handler=self.lam,
+                                   execute_on_handler_change=True,
+                                   )
