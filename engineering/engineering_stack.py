faea67685d07dad8a7c43c289d5bacb5985f8181
prateek <prateek.patel@bricklane.com>
2023-04-16 23:38:44 +0100

    changing to tight coupling between fetch_data and predict

--- engineering/engineering_stack.py
+++ engineering/engineering_stack.py
@@ -1,10 +1,12 @@
 import os
-from aws_cdk import aws_lambda, Duration, Stack
 from constructs import Construct
-from aws_cdk import (aws_events,
+from aws_cdk import (aws_lambda,
+                     Duration,
+                     Stack,
+                     aws_events,
                      aws_events_targets,
-                     aws_apigateway as api_gw,
-                     aws_iam as iam)
+                     aws_lambda_destinations,
+                     )
 
 ENV = os.environ["ENV"]
 
@@ -16,14 +18,33 @@ class DataEngineeringStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create new Container Image.
+        # Predictor Lambda
+        ecr_image = aws_lambda.EcrImageCode.from_asset_image(
+            directory=os.path.join(os.getcwd(), "./engineering"),
+            cmd=["predict_suffer_score.handler"],
+            file="engineering.Dockerfile",
+        )
+
+        predictor_lambda = aws_lambda.Function(self,
+                                               id="strava_predict_suffer_score",
+                                               description="Make predictions on fetched data from Strava",
+                                               code=ecr_image,
+                                               handler=aws_lambda.Handler.FROM_IMAGE,
+                                               runtime=aws_lambda.Runtime.FROM_IMAGE,
+                                               architecture=architecture_map[ENV],
+                                               function_name="strava_suffer_score_predictor",
+                                               memory_size=128,
+                                               reserved_concurrent_executions=2,
+                                               timeout=Duration.seconds(120)
+                                               )
+
+        # Data Fetch Lambda
         ecr_image = aws_lambda.EcrImageCode.from_asset_image(
             directory=os.path.join(os.getcwd(), "./engineering"),
             cmd=["fetch_data.handler"],
             file="engineering.Dockerfile",
         )
 
-        # Lambda Function
         data_fetch_lambda = aws_lambda.Function(
             self,
             id="fetch_data_strava",
@@ -43,11 +64,9 @@ class DataEngineeringStack(Stack):
             memory_size=256,
             reserved_concurrent_executions=2,
             timeout=Duration.seconds(120),
+            on_success=aws_lambda_destinations.LambdaDestination(predictor_lambda,
+                                                                 response_only=True),
         )
-
-        event_policy = iam.PolicyStatement(effect=iam.Effect.ALLOW, resources=['*'], actions=['events:PutEvents'])
-        data_fetch_lambda.add_to_role_policy(event_policy)
-
 
         lambda_schedule = aws_events.Schedule.cron(hour="4", minute="0", year="*", month="*", day="*")
         event_lambda_target = aws_events_targets.LambdaFunction(handler=data_fetch_lambda)
@@ -59,30 +78,3 @@ class DataEngineeringStack(Stack):
             schedule=lambda_schedule,
             targets=[event_lambda_target])
 
-        # Consumer Lambda
-        # Create new Container Image.
-        ecr_image = aws_lambda.EcrImageCode.from_asset_image(
-            directory=os.path.join(os.getcwd(), "./engineering"),
-            cmd=["predict_suffer_score.handler"],
-            file="engineering.Dockerfile",
-        )
-
-        predictor_lambda = aws_lambda.Function(self,
-                                                id="strava_predict_suffer_score",
-                                                description="Make predictions on fetched data from Strava",
-                                                code=ecr_image,
-                                                handler=aws_lambda.Handler.FROM_IMAGE,
-                                                runtime=aws_lambda.Runtime.FROM_IMAGE,
-                                                architecture=architecture_map[ENV],
-                                                function_name="strava_suffer_score_predictor",
-                                                memory_size=128,
-                                                reserved_concurrent_executions=2,
-                                                timeout=Duration.seconds(120)
-                                               )
-
-        event_consumer_rule = aws_events.Rule(self,
-                                              'eventConsumerLambdaRule',
-                                              description='Predict on fetched activities from Strava',
-                                              event_pattern=aws_events.EventPattern(source=['strava.activities']))
-
-        event_consumer_rule.add_target(aws_events_targets.LambdaFunction(handler=predictor_lambda))
