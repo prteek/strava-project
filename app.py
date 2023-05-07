609fbf0d2b0ada8b8a057c76a0bc17d13b3d1bae
prateek <prateek.patel@bricklane.com>
2023-05-07 10:59:57 +0100

    orchestrating training pipeline via cdk and pipeline definition

--- app.py
+++ app.py
@@ -2,14 +2,14 @@
 
 import aws_cdk as cdk
 from engineering.engineering_stack import DataEngineeringStack
-from ml.ml_stack import ModelDeployerStack, PipelineOrchestrationStack
+from ml.ml_stack import ModelDeployerStack, StravaTrainProdPipelineStack
 
 app = cdk.App()
 env = {"region": "eu-west-1"}
 
 lfs = DataEngineeringStack(app, "DataEngineeringStack", env=env)
 mls = ModelDeployerStack(app, "ModelDeployerStack", env=env)
-pos = PipelineOrchestrationStack(app, "PipelineOrchestrationStack", env=env)
+pos = StravaTrainProdPipelineStack(app, "StravaTrainProdPipelineStack", env=env)
 
 app.synth()
 # %%
--- ml/ml_stack.py
+++ ml/ml_stack.py
@@ -1,9 +1,9 @@
 import os
-from aws_cdk import aws_lambda, Duration, Stack, triggers, aws_iam
+from aws_cdk import aws_lambda, Duration, Stack, aws_iam, aws_sagemaker as sm, aws_events, aws_events_targets
 from constructs import Construct
 
 ENV = os.environ["ENV"]
-
+SM_ROLE = os.environ["SAGEMAKER_EXECUTION_ROLE"]
 architecture_map = {"dev": aws_lambda.Architecture.ARM_64,
                     "prod": aws_lambda.Architecture.X86_64}
 
@@ -37,7 +37,7 @@ class ModelDeployerStack(Stack):
             handler=aws_lambda.Handler.FROM_IMAGE,
             runtime=aws_lambda.Runtime.FROM_IMAGE,
             architecture=architecture_map[ENV],
-            environment={"SAGEMAKER_EXECUTION_ROLE": os.environ["SAGEMAKER_EXECUTION_ROLE"]},
+            environment={"SAGEMAKER_EXECUTION_ROLE": SM_ROLE},
             function_name="strava_model_deployer",
             memory_size=128,
             reserved_concurrent_executions=2,
@@ -46,48 +46,52 @@ class ModelDeployerStack(Stack):
         )
 
 
-class PipelineOrchestrationStack(Stack):
+class StravaTrainProdPipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create new Container Image.
-        ecr_image = aws_lambda.EcrImageCode.from_asset_image(
-            directory=os.path.join(os.getcwd(), "./ml"),
-            cmd=["orchestrate_pipeline.handler"],
-            file="ml.Dockerfile",
-        )
+        with open("./ml/training_pipeline_definition.json", encoding="utf8") as fp:
+            pipeline_definition = fp.read()
 
-        role = aws_iam.Role(self, "strava_pipeline_orchestration_role",
+        cfn_pipeline = sm.CfnPipeline(self,
+                                      id="strava-ml-pipeline",
+                                      pipeline_definition={
+                                          "PipelineDefinitionBody": pipeline_definition,
+                                          "PipelineDefinitionS3Location": None
+                                      },
+                                      pipeline_name="strava-ml-pipeline",
+                                      role_arn=SM_ROLE,
+                                      # the properties below are optional
+                                      pipeline_description="ML training pipeline",
+                                      pipeline_display_name="strava-ml-pipeline",
+                                      )
+
+        # Invocation schedule lambda
+
+        with open("./ml/invoke_pipeline_lambda.py", encoding="utf8") as fp:
+            handler_code = fp.read()
+
+        role = aws_iam.Role(self, "strava_training_pipeline_invoke_role",
                             assumed_by=aws_iam.ServicePrincipal("lambda.amazonaws.com"),
                             managed_policies=[lambda_exec_policy,
-                                              sagemaker_full_access_policy,
-                                              s3_full_access_policy
-                                              ]
+                                              sagemaker_full_access_policy]
                             )
 
-        # Lambda Function
-        orchestration_lambda = aws_lambda.Function(
+        lambdaFn = aws_lambda.Function(
             self,
-            id="strava_pipeline_orchestrator",
-            description="Orchestrate Strava ml pipeline on Sagemaker",
-            code=ecr_image,
-            handler=aws_lambda.Handler.FROM_IMAGE,
-            runtime=aws_lambda.Runtime.FROM_IMAGE,
-            architecture=architecture_map[ENV],
-            environment={"SAGEMAKER_EXECUTION_ROLE": os.environ["SAGEMAKER_EXECUTION_ROLE"],
-                         "IMAGE_URI": os.environ["IMAGE_URI"]},
-            function_name="strava_pipeline_orchestrator",
-            memory_size=128,
-            reserved_concurrent_executions=2,
-            timeout=Duration.seconds(120),
+            id="invoke_strava_training_pipeline",
+            function_name="invoke_strava_training_pipeline",
+            code=aws_lambda.InlineCode(handler_code),
+            handler="index.invoke_training_pipeline_handler",
+            timeout=Duration.seconds(60),
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
             role=role,
         )
 
-        if ENV == "prod":
-            # Trigger the function after it is deployed to orchestrate the pipeline
-            trigger = triggers.Trigger(self, "start_orchestration_pipeline",
-                                       handler=orchestration_lambda,
-                                       execute_on_handler_change=True,
-                                       )
-        else:
-            pass  # Do not update pipeline for dev automatically
+        rule = aws_events.Rule(
+            self, "trigger_avm_training_pipeline_rule",
+            schedule=aws_events.Schedule.rate(Duration.days(7)),
+            targets=[aws_events_targets.LambdaFunction(lambdaFn)],
+            enabled=True,
+        )
+
