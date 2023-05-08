81d8e580949e6f2b969cefe59b08c89efbc1fcc5
prateek <prateek.patel@bricklane.com>
2023-05-08 13:58:46 +0100

    model for fitness curve build and decay

--- ml/playground.py
+++ ml/playground.py
@@ -41,8 +41,12 @@ df_activities = (wr
                  .sort_values("date")
                  )
 
-df_merged = pd.merge(df_fitness, df_activities,
-                     on="date",how="left")
+df_merged = (pd
+             .merge(df_fitness, df_activities,
+                     on="date",how="outer")
+             .astype({"fitness_score": float, "date": "datetime64[ns]",
+                      "id": float, "suffer_score": float})
+             )
 
 
 plt.plot(df_merged['date'].astype("datetime64"), df_merged['fitness_score'], label="Fitness date")
@@ -51,4 +55,86 @@ plt.scatter(df_merged['start_timestamp'].dt.date.astype("datetime64"),
             label="Act date")
 plt.legend()
 plt.xticks(rotation=90);
+
+
+df_rest = (df_merged
+          .get(['fitness_score','id', 'date'])
+          .assign(days_diff=lambda x: (x['date'].shift(-1) - x['date']).dt.days,
+                  fitness_score_final=lambda x: x['fitness_score'].shift(-1),
+                  activity_in_between=lambda x: np.logical_not(x['id'].shift(-1).isna()))
+          .query("activity_in_between == False")
+          .dropna(subset=['days_diff', "fitness_score_final"])
+          # .drop(["activity_in_between", "id", "date"], axis=1)
+        )
+
+plt.scatter(df_rest['days_diff'], df_rest['fitness_score_final']-df_rest['fitness_score'])
+plt.xlim(0,10)
+plt.ylim(-4,1)
+
+
+def decay_model(ini, t, params):
+    """Exponential decay base model"""
+    b = params[0]
+    c = params[1]
+    return ini * np.exp(-b * t) + c
+
+
+def exponential_decay_model(x, b, c):
+    """Exponential decay model function to fit"""
+    ini = x[:, 0]
+    t = x[:, 1]
+    return decay_model(ini, t, [b, c])
+
+
+from scipy.optimize import curve_fit
+
+x = np.c_[df_rest['fitness_score'], df_rest['days_diff']]
+y = df_rest['fitness_score_final']
+
+popt, pcov = curve_fit(exponential_decay_model, x, y)
+
+
+t = np.arange(0,35)
+plt.plot(t, decay_model(6.2, t, popt), '--', label='Fitted model')
+plt.scatter([0, 34], [6.2, 2.3], label='real data', s=20, alpha=0.8, c='yellow')
+t = np.arange(0,8)
+plt.plot(t, decay_model(7.08, t, popt), '--', label='Fitted model')
+plt.scatter([0, 7], [7.08, 5.93], label='real data', s=20, alpha=0.8, c='yellow')
+plt.xlabel('Days')
+plt.ylabel('Fitness score')
+plt.legend()
+plt.show()
+
+
+plt.scatter(x[:,0], y, label='real data', s=50, alpha=1, c='yellow')
+plt.plot(x[:,0], decay_model(x[:,0], x[:,1], popt), "s", label='Fitted model', alpha=0.8)
+plt.xlabel('Initial fitness score')
+plt.ylabel('Final fitness score')
+plt.title("Prediction performance")
+plt.legend()
+plt.show()
+
+df_plot = (df_merged
+           .assign(fitness_score_initial=lambda x: x['fitness_score'].shift(1),
+                   score_diff=lambda x: x['fitness_score']-x['fitness_score_initial'])
+           # .dropna(subset=['suffer_score'])
+           .get(['suffer_score', 'score_diff', 'fitness_score_initial', 'fitness_score', 'date'])
+          )
+
+plt.scatter(df_plot['suffer_score'], df_plot['score_diff'])
+
+from sklearn.linear_model import Ridge
+model = Ridge(alpha=1.0)
+
+PREDICTORS = ['suffer_score', 'fitness_score_initial']
+TARGET = 'fitness_score'
+
+df = df_plot.dropna(subset=[TARGET, *PREDICTORS])
+X = df[PREDICTORS].values
+y = df[TARGET].values
+
+model.fit(X, y)
+
+plt.scatter(y, model.predict(X))
+
 
