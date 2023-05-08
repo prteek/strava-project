f733a41024bba0264e483d4ba10c45aeab8d68c3
prateek <prateek.patel@bricklane.com>
2023-05-08 21:51:00 +0100

    adding fitness training pipeline stack

--- ml/app.py
+++ ml/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-from ml_stacks import ModelDeployerStack, StravaTrainProdPipelineStack
+from ml_stacks import ModelDeployerStack, StravaTrainProdPipelineStack, FitnessTrainProdPipelineStack
 
 app = cdk.App()
 env = {"region": "eu-west-1"}
 
 mls = ModelDeployerStack(app, "ModelDeployerStack", env=env)
 pos = StravaTrainProdPipelineStack(app, "StravaTrainProdPipelineStack", env=env)
+ftps = FitnessTrainProdPipelineStack(app, "FitnessTrainProdPipelineStack", env=env)
 
 app.synth()
 # %%
--- ml/ml_stacks.py
+++ ml/ml_stacks.py
@@ -89,7 +89,58 @@ class StravaTrainProdPipelineStack(Stack):
         )
 
         rule = aws_events.Rule(
-            self, "trigger_avm_training_pipeline_rule",
+            self, "trigger_strava_training_pipeline_rule",
+            schedule=aws_events.Schedule.rate(Duration.days(30)),
+            targets=[aws_events_targets.LambdaFunction(lambdaFn)],
+            enabled=True,
+        )
+
+
+
+class FitnessTrainProdPipelineStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        with open("./fitness_training_pipeline_definition.json", encoding="utf8") as fp:
+            pipeline_definition = fp.read()
+
+        cfn_pipeline = sm.CfnPipeline(self,
+                                      id="fitness-ml-pipeline",
+                                      pipeline_definition={
+                                          "PipelineDefinitionBody": pipeline_definition,
+                                          "PipelineDefinitionS3Location": None
+                                      },
+                                      pipeline_name="fitness-ml-pipeline",
+                                      role_arn=SM_ROLE,
+                                      # the properties below are optional
+                                      pipeline_description="Fitness ML training pipeline",
+                                      pipeline_display_name="fitness-ml-pipeline",
+                                      )
+
+        # Invocation schedule lambda
+
+        with open("./invoke_pipeline_lambda.py", encoding="utf8") as fp:
+            handler_code = fp.read()
+
+        role = aws_iam.Role(self, "fitness_training_pipeline_invoke_role",
+                            assumed_by=aws_iam.ServicePrincipal("lambda.amazonaws.com"),
+                            managed_policies=[lambda_exec_policy,
+                                              sagemaker_full_access_policy]
+                            )
+
+        lambdaFn = aws_lambda.Function(
+            self,
+            id="invoke_fitness_training_pipeline",
+            function_name="invoke_fitness_training_pipeline",
+            code=aws_lambda.InlineCode(handler_code),
+            handler="index.invoke_fitness_training_pipeline_handler",
+            timeout=Duration.seconds(60),
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            role=role,
+        )
+
+        rule = aws_events.Rule(
+            self, "trigger_fitness_training_pipeline_rule",
             schedule=aws_events.Schedule.rate(Duration.days(30)),
             targets=[aws_events_targets.LambdaFunction(lambdaFn)],
             enabled=True,
