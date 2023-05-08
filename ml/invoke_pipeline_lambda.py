b6a420cba03237ef45ec4016f01b0efbd7a4be57
prateek <prateek.patel@bricklane.com>
2023-05-08 21:52:37 +0100

    adding invocation handler for fitness pipeline

--- ml/invoke_pipeline_lambda.py
+++ ml/invoke_pipeline_lambda.py
@@ -17,3 +17,17 @@ def invoke_training_pipeline_handler(event, context=None):
     )
 
     return response
+
+
+def invoke_fitness_training_pipeline_handler(event, context=None):
+    """ Lambda handler to invoke fitness training pipeline"""
+    pipeline_name = "strava-fitness-pipeline"
+    invocation_timestamp = datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
+
+    response = client.start_pipeline_execution(
+        PipelineName=pipeline_name,
+        PipelineExecutionDisplayName="fitness-training-pipeline-execution-" + invocation_timestamp,
+        PipelineExecutionDescription="Triggered by lambda function",
+    )
+
+    return response
