12e15014bbba9940f1ffb57bddf2f0381108dd92
prateek <prateek.patel@bricklane.com>
2023-05-18 09:02:38 +0100

    reverting back to dual model system for better behaved outputs

--- ml/train_fitness_model.py
+++ ml/train_fitness_model.py
@@ -5,7 +5,7 @@ import pandas as pd
 import os
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDRegressor
-from sklearn.preprocessing import StandardScaler, FunctionTransformer as FT
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 import numpy as np
 from logger import logger
@@ -15,11 +15,7 @@ from helpers import (
     FitnessModel,
     PREDICTORS_FITNESS,
     expected_error,
-    dtype_converter,
 )
-from torch import nn
-import torch
-from skorch import NeuralNetRegressor
 
 
 if __name__ == "__main__":
@@ -51,50 +47,41 @@ if __name__ == "__main__":
     )
 
     X = df_train[PREDICTORS_FITNESS]
-    y = df_train[TARGET].values.astype(np.float32)
+    y = df_train[TARGET]
 
     logger.info("Training model")
-    torch.manual_seed(42)
-    estimator_ = nn.Sequential(
-        nn.Linear(3, 50),
-        nn.Tanh(),
-        nn.Dropout(0.2),
-        nn.Linear(50, 50),
-        nn.Tanh(),
-        nn.Dropout(0.2),
-        nn.Linear(50, 50),
-        nn.Tanh(),
-        nn.Linear(50, 2)
-    )
-
-    estimator = NeuralNetRegressor(
-        estimator_,
-        max_epochs=1000,
-        criterion=nn.MSELoss(),
-        lr=0.01,
-        # Shuffle training data on each epoch
-        iterator_train__shuffle=False,
-    )
-
-    model = Pipeline(
+    rest_model = ExponentialDecayEstimator()
+    activity_model = Pipeline(
         [
             ("scaler", StandardScaler()),
-            ("dtype_converter", FT(dtype_converter)),
-            ("estimator", estimator),
+            (
+                "estimator",
+                SGDRegressor(
+                    loss="squared_error",
+                    penalty="l2",
+                    alpha=0.01,
+                    max_iter=100,
+                    tol=1e-3,
+                    random_state=42,
+                    eta0=0.1,
+                    verbose=1,
+                ),
+            ),
         ]
     )
 
+    model = FitnessModel(rest_model=rest_model, activity_model=activity_model)
     model.fit(X, y)
 
     # Emit the required metrics
-    y_true = y[:, 0]
+    y_true = y.values[:, 0]
     y_pred = model.predict(X)[:, 0]
     rmse = round(np.sqrt(mean_squared_error(y_true, y_pred)))
     print(f"train_rmse: {rmse}")
     print(f"train_mean_error: {round(expected_error(y_true, y_pred))}")
     print(f"train_r2_score: {round(r2_score(y_true, y_pred), 3)}")
 
-    y_true = y[:, 1]
+    y_true = y.values[:, 1]
     y_pred = model.predict(X)[:, 1]
     rmse = round(np.sqrt(mean_squared_error(y_true, y_pred)))
     print(f"train_rmse: {rmse}")
