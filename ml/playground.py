c76800ce05fb51cdd6460333e1f484c5d6be06ae
prateek <prateek.patel@bricklane.com>
2023-05-22 14:22:29 +0100

    diff eq constraint

--- ml/playground.py
+++ ml/playground.py
@@ -104,3 +104,92 @@ x = pd.DataFrame(np.c_[ini, d, ss], columns=PREDICTORS_FITNESS)
 plt.plot(d, model.predict(x)[:,0])
 plt.plot(d, ini*np.exp(-0.028*d))
 # plt.scatter([0,35], [6.2,2.3])
+
+
+
+
+import torch
+import torch.nn as nn
+from torch import optim
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define the NN model to solve the problem
+class Model(nn.Module):
+    def __init__(self, n_units=10, n_hidden=2):
+        super(Model, self).__init__()
+        self.lin_in = nn.Linear(1,n_units)
+        self.hidden = [nn.Linear(n_units,n_units) for i in range(n_hidden)]
+        self.lin_out = nn.Linear(n_units,1)
+
+    def forward(self, x):
+        x = torch.relu(self.lin_in(x))
+        for i_hidden in self.hidden:
+            x = torch.relu(i_hidden(x))
+
+        x = self.lin_out(x)
+        return x
+
+
+
+model = Model()
+
+torch.random.manual_seed(0)
+x_ = 2*(torch.rand(size=(10,1))-0.5)
+noise = (torch.rand(size=(10,1)) - 0.5)*0.05
+y_ = torch.exp(- x_) + noise
+
+# plt.scatter(x_.detach().numpy(), y_.detach().numpy())
+# plt.show()
+# Define loss_function from the Ordinary differential equation to solve
+
+
+def ODE(x,y):
+    # There is a small precaution in using the gradient with vector input.
+    # You should feed the backward function with unit vector in order to access the gradient as a vector.
+    dydx, = torch.autograd.grad(y, x,
+                                grad_outputs=torch.ones_like(y),
+                                create_graph=True, # Needed since the ODE function itself is differentiated further making this step twice differentiable
+                                )
+
+    eq = dydx + y  # y' = - 2x*y
+    ic = (model(x_) - y_)**2  # y(x=0) = 1
+    long_range_decay = (model(torch.tensor([[100.0]])) - 0.0)**2  # y(x=100) = 0
+    return torch.mean(ic)*5 + torch.mean(eq**2)*1 + long_range_decay*0
+
+
+loss_func = ODE
+
+# Define the optimization
+opt = optim.Adam(model.parameters(), lr=0.005)
+
+# Define reference grid
+x_data_ = torch.linspace(-2,10,100).view(-1,1)
+x_data = x_data_.data.new(x_data_).requires_grad_(True)
+# Iterative learning
+epochs = 5000
+for epoch in range(epochs):
+    torch.random.manual_seed(0)
+    opt.zero_grad()
+    y_trial = model(x_data)
+    loss = loss_func(x_data, y_trial)
+
+    loss.backward()  # This is where the gradient is calculated wrt the parameters and x values
+    opt.step()
+
+    if epoch % 100 == 0:
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
+
+
+y_data = model(x_data)
+x = torch.linspace(-2,10,1000).view(-1,1)
+y = model(x)
+# Plot Results
+plt.scatter(x_.data.numpy(), y_.data.numpy(), label='data')
+plt.plot(x, np.exp(-x), label='exact')
+plt.plot(x, y.data, label='approx')
+plt.fill_between([x_.data.min(), x_.data.max()], 0,5, label='training range', alpha=0.2)
+plt.legend()
+plt.show()
+
+
