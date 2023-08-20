1f60e9690dce6f169deec464a3aca7cb64184606
prteek <prteek>
2023-08-20 15:10:10 +0000

    creating table from csv

---
+++ engineering/ingest_data.py
@@ -0,0 +1,23 @@
+# File location and type
+file_location = "/user/hive/warehouse/activities.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "true"
+first_row_is_header = "true"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+display(df)
+
+
+# Create a table
+permanent_table_name = "activities"
+
+df.write.format("parquet").saveAsTable(permanent_table_name)
